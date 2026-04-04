e7f2fe29000527bbc2fdbbb20cf9d03fd78b257e
Robin Voetter <rvoetter@amd.com>
2026-04-04 21:50:57 +0000

    [rocprofiler-sdk] allow using system elfio dependency
    This adds a new option, ROCPROFILER_BUILD_ELFIO, which can be set to OFF to
    make rocprofiler use a system dependency for ELFIO instead of trying to build
    it manually. To mimick old behavior, ROCPROFILER_BUILD_ELFIO is set to ON by
    default.
    
    Related: https://github.com/ROCm/rocm-systems/pull/2319

--- projects/rocprofiler-sdk/cmake/rocprofiler_options.cmake
+++ projects/rocprofiler-sdk/cmake/rocprofiler_options.cmake
@@ -71,6 +71,8 @@ rocprofiler_add_option(ROCPROFILER_BUILD_GOTCHA
                        "Enable building gotcha library internally" ON)
 rocprofiler_add_option(ROCPROFILER_BUILD_YAML_CPP
                        "Enable building yaml-cpp library internally" ON)
+rocprofiler_add_option(ROCPROFILER_BUILD_ELFIO "Enable building ELFIO library internally"
+                       ON)
 if(ROCPROFILER_BUILD_TESTS)
     rocprofiler_add_option(
         ROCPROFILER_BUILD_GTEST
