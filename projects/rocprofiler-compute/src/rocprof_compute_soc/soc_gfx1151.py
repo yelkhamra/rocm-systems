a56bf8284b5b03acef6f3e584b7703b2c377b116
Vignesh Edithal <Vignesh.Edithal@amd.com>
2026-04-06 05:25:03 +0000

    Remove dead code from soc_gfx1151.py

--- projects/rocprofiler-compute/src/rocprof_compute_soc/soc_gfx1151.py
+++ projects/rocprofiler-compute/src/rocprof_compute_soc/soc_gfx1151.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier:  MIT
 
 import argparse
-from typing import Any, Optional
+from typing import Optional
 
 from rocprof_compute_soc.soc_base import OmniSoC_Base
 from utils.logger import demarcate
@@ -38,10 +38,3 @@ class gfx1151_soc(OmniSoC_Base):
     def post_profiling(self) -> None:
         """Perform any SoC-specific post profiling activities."""
         super().post_profiling()
-
-    @demarcate
-    def analysis_setup(
-        self, roofline_parameters: Optional[dict[str, Any]] = None
-    ) -> None:
-        """Perform any SoC-specific setup prior to analysis."""
-        super().analysis_setup(roofline_parameters=roofline_parameters)
