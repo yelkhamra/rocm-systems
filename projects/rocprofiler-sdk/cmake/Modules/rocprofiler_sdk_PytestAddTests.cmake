39f74e0e31f86850ab24ec1e4768e967dbf19ed1
Robin Voetter <rvoetter@amd.com>
2026-04-15 18:24:49 +0000

    [rocprofiler-sdk] update all other cmake_minimum_required to 3.24

--- projects/rocprofiler-sdk/cmake/Modules/rocprofiler_sdk_PytestAddTests.cmake
+++ projects/rocprofiler-sdk/cmake/Modules/rocprofiler_sdk_PytestAddTests.cmake
@@ -1,5 +1,5 @@
 # Wrapper used to create individual CTest tests from Pytest tests.
-cmake_minimum_required(VERSION 3.20...4.1)
+cmake_minimum_required(VERSION 3.24...4.1)
 
 if(CMAKE_SCRIPT_MODE_FILE)
 
--- projects/rocprofiler-sdk/source/scripts/run-ci.py
+++ projects/rocprofiler-sdk/source/scripts/run-ci.py
@@ -308,7 +308,7 @@ def generate_dashboard_script(args):
         MEMCHECK = 0
 
     _script = f"""
-        cmake_minimum_required(VERSION 3.21 FATAL_ERROR)
+        cmake_minimum_required(VERSION 3.24 FATAL_ERROR)
 
         macro(dashboard_submit)
             if("{SUBMIT}" GREATER 0)
